7d4dc26dbb7859423033ff946ea1e6ab0648c5ab
dreaca <buddhikadreaca@gmail.com>
2024-11-26 16:06:58 +0530

    import stuff

---
+++ Day4/day4-modules/main.py
@@ -0,0 +1,16 @@
+# import my_module importing module using my_module
+# from my_module import add #importing only add function
+from my_module import * #importing all functions in module 
+import math as m
+
+# print(my_module.add(10,20)) 
+
+print(add(10,20))
+
+print(subtract(29,10))
+
+# print(m.pi)
+
+# print(m.factorial(10))
+
+# math can also be called with a nickname using "as"
