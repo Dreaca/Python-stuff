206884f25a9d31a26799b76f291675707d654454
dreaca <buddhikadreaca@gmail.com>
2024-12-17 09:36:43 +0530

    Libraries

---
+++ Day07/libraries.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4,5])
+b = np.array([6, 7, 8, 9, 10])
+
+# result = a + b
+
+# print("Add Result :",result)
+
+# sub_result = b - a
+
+# print("Sub Result :",sub_result)
+
+# mul_result = a * b
+
+# print("Multi Result :",mul_result)
+
+# div_result = b/a
+
+# print("Div result :",div_result)
+
+result = np.sqrt(a)
+
+print(result)
