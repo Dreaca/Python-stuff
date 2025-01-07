f9cdca20c30c89d8e984feab6531fc5be6ef00ac
dreaca <buddhikadreaca@gmail.com>
2025-01-07 08:40:30 +0530

    pandas operations

--- Day07/ex.py
+++ Day07/ex.py
@@ -22,21 +22,46 @@
 #2. Create 1D a numpy array with elements 10,20,30,40,50 . Use boolean indexing to extract all elements greater than the mean 
 #of the array.
 
-import numpy as np
+# import numpy as np
 
-array = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+# array = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 
-even_numbers = array[array%2 == 0]
+# even_numbers = array[array%2 == 0]
 
-print("Even numbers:", even_numbers)
+# print("Even numbers:", even_numbers)
 
-array_2 = np.array([10,20,30,40,50])
+# array_2 = np.array([10,20,30,40,50])
 
-mean_value = np.mean(array_2)
+# mean_value = np.mean(array_2)
 
-print("Mean value:", mean_value)
+# print("Mean value:", mean_value)
 
-filtered = array_2[array_2 > mean_value]
+# filtered = array_2[array_2 > mean_value]
 
-print(filtered)
+# print(filtered)
+
+#================================================================================================================================#
+# You are a data analyst for a data company. The Company have provided you with the above table containing sales data
+#you  task is to analyze the sales data using python and pandas to answer some key business questions.
+#1. Calculate the total revenue for each order : Add a new column total revenue to store them
+#2. Identify the best selling product : find the product with the highest total sales revenue
+import pandas as pd
+data ={
+    "Order Id": [101,102,103,104,105],
+    "Product":["Laptop","Smart Phone","Desk Chair","Monitor","Book Shelf"],
+    "Category":["Electronics","Electronics","Furniture","Electronics","Furniture"],
+    "Quantity" :[2,5,10,4,2],
+    "Price per Unit" :[1000,800,150,200,300],
+    "Region":["North","South","East","West","North"]
+}
+
+df = pd.DataFrame(data)
+
+df["Total Revenue"] = df["Quantity"] * df["Price per Unit"]
+
+print(df)
+df_max = df[df["Total Revenue"] == df["Total Revenue" ].max()]
+print(df_max)
+
+# print("\nBest Selling Product:",df[])
 
--- Day07/pandas-thing.py
+++ Day07/pandas-thing.py
@@ -1,21 +1,89 @@
 import pandas as pd
+{
+# data = {
+#     "Name": ["Alice", "Bob", "Charlie", "David"],
+#     "Age": [24, 30, 29, 27]
+# }
 
-data = {
-    "Name": ["Alice", "Bob", "Charlie", "David"],
-    "Age": [24, 30, 29, 27]
+# df = pd.DataFrame(data)
+
+# #Locating cell by row index and column label
+# # print(df.loc[1, "Name"])
+
+# # #locating cell by row index and column index
+# # print(df.iloc[1,0])
+
+# # #Selecting cell 
+# # print(df.loc[1])
+
+# #Selecting multiple rows
+
+# print(df.loc[[0,2]])
+
+
+# data = {
+#     "Calories" : [420,380,390]
+#     ,"duration":[50,40,45]
+
+# df = pd.DataFrame(data, index=["day1","day2","day3"]) #giving indexes labels 
+
+# print(df)
+
+# print(df.loc["day1"]) selecting rows from labels
+
+# print(df.loc["day2":"day3"]) # selecting multiple rows
+
+# print(df["Calories"])
+
+# print(df.shape)
+
+# print(df.columns)
+
+# print(list(df.columns))
+
+# print(df.size)
+
+# print(df.values) returns values as a numpy array
+
+# print(len(df)) returns the number of rows
+
+# print(df.mean()) returns the mean of all columns
+
+# print(df.sum()) returns the sum of all values in each column
+
+# print(df.describe()) returns the statistic for each column
+
+#Filtering rows in a DataFrame
+
+# data = {
+#     "Name": ["Alice", "Bob", "Charlie", "David"],
+#     "Age": [24, 30, 29, 27]
+# }
+
+# df = pd.DataFrame(data)
+
+# filtered = df[df["Age"]>25]
+
+# print(filtered, type(filtered))
+
+#Adding a new row 
+
+# df.loc[len(df)] = ["John", 25]
+
+# print(df, type(df))
+
+#Deleting a Column
+
+# print (df.drop('Age', axis=1))
+
+#Deleting a row
+
+# print(df.drop(3, axis=0))
 }
+df = pd.read_csv("Day07/customers-100.csv")
 
-df = pd.DataFrame(data)
+print(df)
 
-#Locating cell by row index and column label
-# print(df.loc[1, "Name"])
+djson = pd.read_json("Day07/student.json")
 
-# #locating cell by row index and column index
-# print(df.iloc[1,0])
-
-# #Selecting cell 
-# print(df.loc[1])
-
-#Selecting multiple rows
-
-print(df.loc[[0,2]])
+print(djson)
